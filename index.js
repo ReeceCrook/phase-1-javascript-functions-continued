c359ba505101fcd0dfc7c832f79341e848840fa3
Reece Crook <reececrook16@gmail.com>
2023-03-14 18:28:45 -0600

    Completed Lab

--- index.js
+++ index.js
@@ -1,1 +1,20 @@
 // code your solution here
+function saturdayFun(activity = "roller-skate"){
+    let message = (`This Saturday, I want to ${activity}!`);
+    return message
+
+}
+
+const mondayWork = function (activity = "go to the office") {
+    let message = `This Monday, I will ${activity}.`
+    return message
+
+}
+
+function wrapAdjective(activity){
+    return function (paramiter = "special") {
+        let message = `You are ${activity}${paramiter}${activity}!`
+        return message
+    }
+
+}
